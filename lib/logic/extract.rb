ff070d7f47aa3a4f1e8cd44fe79488c8332050ce
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-10 10:09:25 +0100

    add extract code title logic

---
+++ lib/logic/extract.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+
+def load_xml path
+  Nokogiri.Slop(File.open(path).read)
+end
+
+def load_version_xmls path
+  Dir.glob(path + '/**/version/*.xml').map do |version_file|
+    f = File.open(version_file)
+    xml = Nokogiri.Slop(f.read)
+    f.close()
+    xml
+  end
+end
+
+def get_code_title xml
+  xml.TEXTE_VERSION.META.META_SPEC.META_TEXTE_VERSION.TITRE.content
+end
+
+def get_code_titles path
+  version_xmls = load_version_xmls(path)
+  code_titles = version_xmls.map { |version_xml| get_code_title (version_xml)}
+  code_titles.uniq!.sort!
+end
---
+++ spec/lib/logic/extract_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+require './lib/logic/extract'
+
+describe 'extraction of versions files' do
+
+  LEGI_ROOT_PATH = './spec/lib/logic/legi/'
+
+  describe 'with a directory with 3 version files' do
+    it "extrait 3 path" do
+      expect(load_version_xmls(LEGI_ROOT_PATH).length).to eq(3)
+    end
+  end
+
+  describe 'when we have one version file' do
+    it 'extracts the title code correctly' do
+      version_file = LEGI_ROOT_PATH + 'LEGITEXT000005627819/texte/version/LEGITEXT000005627819.xml'
+      code_titre = get_code_title(load_xml (version_file))
+      expect(code_titre).to  eq('Code des marchés publics')
+    end
+  end
+
+  describe 'when we have 3 version files but 2 uniq' do
+    it 'extracts the 2 title code correctly' do
+      codes_titres = get_code_titles(LEGI_ROOT_PATH)
+      expect(codes_titres.length).to  eq(2)
+    end
+  end
+
+  describe 'test local file' do
+    before do
+      @xml = <<-FOO
+<?xml version="1.0" encoding="UTF-8"?>
+<TEXTE_VERSION>
+<META>
+    <META_SPEC>
+      <META_TEXTE_VERSION>
+        <TITRE>Code des marchés publics</TITRE>
+      </META_TEXTE_VERSION>
+    </META_SPEC>
+  </META>
+</TEXTE_VERSION>
+FOO
+    end
+
+    it 'extracts the title code correctly' do
+      code_titre = get_code_title(Nokogiri.Slop(@xml))
+      expect(code_titre).to  eq('Code des marchés publics')
+    end
+
+  end
+
+end
