ed9f51d4f748d08e4d5628573eec84bf836808f1
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-15 21:54:00 +0100

    code summary: use hash to improve perf during graph construction
    improve perf *10

--- app/models/code.rb
+++ app/models/code.rb
@@ -2,22 +2,19 @@ class Code < ActiveRecord::Base
   has_many :sections, :dependent => :delete_all
 
   def summary
+    hash = {}
+
     sections_vigueur = sections.where(state: 'VIGUEUR')
-    level1 = sections_vigueur.select{ |s| s.level == 1 }.sort_by { |s| s.order }
-    level1.each{|l1| fetch_children(l1, sections_vigueur)}
-    level1
-  end
+      .sort_by { |s| s.order }
+      .each { |s| hash[s.id_section_origin] = s }
 
-
-  private
-  def fetch_children(father, sections)
-    children = sections.select{|s| s.level == (father.level + 1) && s.id_section_parent_origin == father.id_section_origin }.sort_by{|s| s.order}
-
-    if !children.empty?
-      children.each{|c| fetch_children(c, sections)}
+    sections_vigueur.each do |s|
+      next if s.id_section_parent_origin.nil? || hash[s.id_section_parent_origin].nil?
+      hash[s.id_section_parent_origin].sections = [] if hash[s.id_section_parent_origin].sections.nil?
+      hash[s.id_section_parent_origin].sections.push(s)
     end
 
-    father.sections = children
+    sections_vigueur.select{ |s| s.level == 1 }
   end
 
 end
