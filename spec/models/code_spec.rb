a9cb96ebd788ca687c383013a9c719d3da12ca89
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-17 11:19:09 +0100

    refactor code summary spec

--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -4,157 +4,96 @@ RSpec.describe Code, type: :model do
 
   describe "summary" do
 
-    describe "with a code with one section" do
-      before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "id", state: "VIGUEUR", order: 0})
-        code.sections.push(@section_1)
+    def createCodeWithSection options=[{}]
+      default = {level: 1, title: "1", id_section_origin: "id1", state: "VIGUEUR", order: 0}
+      section_hashs = options.map{|o| default.merge(o)}
+
+      code = Code.new()
+      section_hashs.each do |h|
+        s=Section.new(h)
+        code.sections.push(s)
         code.save()
+        code
+      end
+      code
+    end
+
+    describe "with a code with two sections vigueur and one not" do
+      before do
+        code = createCodeWithSection [{title:"ok"}, {title:"ok2"}, {title:"nop", state: 'PLOP'}]
         @summary = code.summary
       end
 
       it "displays one section" do
-        expect(@summary).to eq([@section_1])
-      end
-    end
-
-    describe "with a code with one section not in state VIGUEUR" do
-      before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "id", state: 'PLOP'})
-        code.sections.push(@section_1)
-        code.save()
-        @summary = code.summary
-      end
-
-      it "displays no section" do
-        expect(@summary.length).to eq(0)
-      end
-    end
-
-    describe "with a code with 2 sections of level 1" do
-      before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "id1", state: "VIGUEUR", order: 0})
-        @section_2 = Section.new({level: 1, title: "2", id_section_origin: "id2", state: "VIGUEUR", order: 1})
-        code.sections.push(@section_1)
-        code.sections.push(@section_2)
-        code.save()
-        @summary = code.summary
-      end
-
-      it "displays two sections" do
-        expect(@summary).to eq([@section_1, @section_2])
+        expect(@summary.length).to eq(2)
+        expect(@summary[0].title).to eq("ok")
+        expect(@summary[1].title).to eq("ok2")
       end
     end
 
     describe "with a code with 2 sections of level 1 in descending order" do
       before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "id1", state: "VIGUEUR", order: 1})
-        @section_2 = Section.new({level: 1, title: "2", id_section_origin: "id2", state: "VIGUEUR", order: 0})
-        code.sections.push(@section_1)
-        code.sections.push(@section_2)
-        code.save()
+        code = createCodeWithSection [{title:"second", order:2}, {title:"first", order:1}]
         @summary = code.summary
       end
 
       it "displays two sections in ascending order" do
-        expect(@summary).to eq([@section_2, @section_1])
+        expect(@summary[0].title).to eq("first")
+        expect(@summary[1].title).to eq("second")
       end
     end
 
-    describe "with a code with 2 sections of level 1 with second one not in 'vigueur' state" do
-      before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "id1", state: "VIGUEUR", order: 0})
-        @section_2 = Section.new({level: 1, title: "2", id_section_origin: "id2", state: "MODIFIE", order: 1})
-        code.sections.push(@section_1)
-        code.sections.push(@section_2)
-        code.save()
-        @summary = code.summary
-      end
 
-      it "displays only the first section" do
-        expect(@summary).to eq([@section_1])
-      end
-    end
-
-    describe "with a code with 1 section of level 1 and one section of level 2" do
-      before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_1 = Section.new({level: 2, title: "1.1", id_section_origin: "1.1", id_section_parent_origin: "1", state: "VIGUEUR", order: 0})
-        code.sections.push(@section_1)
-        code.sections.push(@section_1_1)
-        code.save()
-        @summary = code.summary
-      end
-
-      it "display one section lvl1 and one lvl2" do
-        expect(@summary).to eq([@section_1])
-        expect(@summary[0].sections[0]).to eq(@section_1_1)
-      end
-
-    end
 
     describe "with a code with 1 section of level 1 and two sections of level 2" do
       before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_1 = Section.new({level: 2, title: "1.1", id_section_origin: "1.1", id_section_parent_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_2 = Section.new({level: 2, title: "1.2", id_section_origin: "1.2", id_section_parent_origin: "1", state: "VIGUEUR", order: 1})
-        code.sections.push(@section_1)
-        code.sections.push(@section_1_1)
-        code.sections.push(@section_1_2)
-        code.save()
+        code = createCodeWithSection [
+                                         {title:"1", order:1, id_section_origin: '1'},
+                                         {title:"1.1", level: 2, order:1, id_section_parent_origin: '1'},
+                                         {title:"1.2", level: 2, order:2, id_section_parent_origin: '1'}]
         @summary = code.summary
       end
 
-      it "display 1 lvl1 and 2 lvl2 in order" do
-        expect(@summary).to eq([@section_1])
+      it "display one section lvl1 and two lvl2" do
+        expect(@summary.length).to eq(1)
         expect(@summary[0].sections.length).to eq(2)
-        expect(@summary[0].sections[1]).to eq(@section_1_2)
+        expect(@summary[0].sections[0].title).to eq("1.1")
+        expect(@summary[0].sections[1].title).to eq("1.2")
       end
+
     end
 
     describe "with a code with 1 section of level 1 and two sections of level 2  in descending order" do
       before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_1 = Section.new({level: 2, title: "1.1", id_section_origin: "1.1", id_section_parent_origin: "1", state: "VIGUEUR", order: 1})
-        @section_1_2 = Section.new({level: 2, title: "1.2", id_section_origin: "1.2", id_section_parent_origin: "1", state: "VIGUEUR", order: 0})
-        code.sections.push(@section_1)
-        code.sections.push(@section_1_1)
-        code.sections.push(@section_1_2)
-        code.save()
+        code = createCodeWithSection [
+                                         {title:"1", order:1, id_section_origin: '1'},
+                                         {title:"1.2", level: 2, order:2, id_section_parent_origin: '1'},
+                                         {title:"1.1", level: 2, order:1, id_section_parent_origin: '1'}]
         @summary = code.summary
       end
 
-      it "display two sections" do
-        expect(@summary).to eq([@section_1])
+      it "display one section lvl1 and two lvl2 in order" do
+        expect(@summary.length).to eq(1)
         expect(@summary[0].sections.length).to eq(2)
-        expect(@summary[0].sections[1]).to eq(@section_1_1)
+        expect(@summary[0].sections[0].title).to eq("1.1")
+        expect(@summary[0].sections[1].title).to eq("1.2")
       end
+
     end
+
 
     describe "with a code with 1 section of level 1 and two sections of level 2 with second one not in 'vigueur' state" do
       before do
-        code = Code.new()
-        @section_1 = Section.new({level: 1, title: "1", id_section_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_1 = Section.new({level: 2, title: "1.1", id_section_origin: "1.1", id_section_parent_origin: "1", state: "VIGUEUR", order: 0})
-        @section_1_2 = Section.new({level: 2, title: "1.2", id_section_origin: "1.2", id_section_parent_origin: "1", state: "MODIFIE", order: 1})
-        code.sections.push(@section_1)
-        code.sections.push(@section_1_1)
-        code.sections.push(@section_1_2)
-        code.save()
+        code = createCodeWithSection [
+                                         {title:"1", order:1, id_section_origin: '1'},
+                                         {title:"1.1", level: 2, order:1, id_section_parent_origin: '1', state: 'NOP'},
+                                         {title:"1.2", level: 2, order:2, id_section_parent_origin: '1'}]
         @summary = code.summary
       end
 
       it "display one section and only the first sub section" do
-        expect(@summary).to eq([@section_1])
         expect(@summary[0].sections.length).to eq(1)
-        expect(@summary[0].sections[0]).to eq(@section_1_1)
+        expect(@summary[0].sections[0].title).to eq("1.2")
       end
     end
 
