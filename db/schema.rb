4870639b19f086648f94daea3db75c75bad91eee
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-18 12:41:39 +0100

    remove section_id to article

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118092259) do
+ActiveRecord::Schema.define(version: 20151118113934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +25,10 @@ ActiveRecord::Schema.define(version: 20151118092259) do
     t.string   "text"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.integer  "section_id"
     t.integer  "order"
     t.string   "number"
     t.string   "nature"
   end
-
-  add_index "articles", ["section_id"], name: "index_articles_on_section_id", using: :btree
 
   create_table "codes", force: :cascade do |t|
     t.string   "title"
@@ -56,6 +53,5 @@ ActiveRecord::Schema.define(version: 20151118092259) do
 
   add_index "sections", ["code_id"], name: "index_sections_on_code_id", using: :btree
 
-  add_foreign_key "articles", "sections"
   add_foreign_key "sections", "codes"
 end
