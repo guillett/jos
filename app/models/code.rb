7e0ec07f6be0b034f5c181020d0fe38b62ef83c3
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-18 17:07:10 +0100

    display section in vigueur and in abroge diff

--- app/models/code.rb
+++ app/models/code.rb
@@ -4,7 +4,7 @@ class Code < ActiveRecord::Base
   def summary
     hash = {}
 
-    sections_vigueur = sections.select{|s| s.state == 'VIGUEUR'}
+    sections_vigueur = sections
       .sort_by { |s| s.order }
       .each do |s|
       hash[s.id_section_origin] = [] if hash[s.id_section_origin].nil?
@@ -19,8 +19,8 @@ class Code < ActiveRecord::Base
     sections_vigueur.select{ |s| s.level == 1 }
   end
 
-  def self.with_vigueur_sections_and_articles escape_title
-    Code.includes(sections: :articles).where(sections: { state: 'VIGUEUR' }).find_by escape_title: escape_title
+  def self.with_displayable_sections_and_articles escape_title
+    Code.includes(sections: :articles).where(sections: { state: ['VIGUEUR','ABROGE_DIFF'] }).find_by escape_title: escape_title
   end
 
 end
