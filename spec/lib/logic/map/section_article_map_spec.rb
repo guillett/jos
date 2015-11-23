37deba9f374366ecc3e4dc43832159342f06fa87
Nissim Naccache <nissimnac@gmail.com>
2015-11-23 18:04:33 +0100

    adapt section_article_map_spec to new model

--- spec/lib/logic/map/section_article_map_spec.rb
+++ spec/lib/logic/map/section_article_map_spec.rb
@@ -10,14 +10,14 @@ describe 'mapping of sections article links' do
   describe "when we have one section link" do
 
     before do
-      @section_article_map = SectionArticleMap.parse(fake_section_article_link)
+      @link_section_article_map = LinkArticleSectionMap.parse(fake_section_article_link)
     end
 
     it 'maps correctly the section article link' do
-      expect(@section_article_map.state).to             eq("MODIFIE")
-      expect(@section_article_map.start_date).to        eq("2002-03-28")
-      expect(@section_article_map.end_date).to          eq("2002-12-14")
-      expect(@section_article_map.id_article_origin).to eq("LEGIARTI000006391999")
+      expect(@link_section_article_map.state).to             eq("MODIFIE")
+      expect(@link_section_article_map.start_date).to        eq("2002-03-28")
+      expect(@link_section_article_map.end_date).to          eq("2002-12-14")
+      expect(@link_section_article_map.target_id_article_origin).to eq("LEGIARTI000006391999")
     end
 
   end
