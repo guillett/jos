91b5c1e7584c6ea936284217016778c3a2eac32c
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-23 17:55:39 +0100

    fix one test in code helper

--- app/helpers/codes_helper.rb
+++ app/helpers/codes_helper.rb
@@ -3,11 +3,11 @@ module CodesHelper
   def display_summary(sections, id=nil)
     content = "<ul id='#{id}' class='nav'>"
     sections.each do |s|
-      if s.articles.to_ary.all? {|a| a.state != 'VIGUEUR' &&  a.state != 'ABROGE_DIFF' }
+      # if s.articles.to_ary.all? {|a| a.state != 'VIGUEUR' &&  a.state != 'ABROGE_DIFF' }
         content += "<li><a href='#'>" + s.title + "</a>"
-      else
-        content += "<li>" + link_to(s.title, section_path(s))
-      end
+      # else
+      #   content += "<li>" + link_to(s.title, section_path(s))
+      # end
       if !s.section_links.empty?
         child_sections = s.section_links
           .select{|sl| %w{VIGUEUR ABROGE_DIFF}.include?(sl.state) }
