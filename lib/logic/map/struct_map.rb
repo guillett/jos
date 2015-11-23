5389f05cea90bc5c2bc3f8ee23847c251d61f76b
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-23 18:06:38 +0100

    clean struct map

--- lib/logic/map/struct_map.rb
+++ lib/logic/map/struct_map.rb
@@ -7,15 +7,6 @@ class StructMap
   has_one :id_section_origin, String, :xpath => 'META/META_COMMUN/ID'
   has_many :section_links, LinkSectionMap, :xpath => 'STRUCT'
 
-  # def extract_linked_sections
-  #   @sections.map.with_index do |s, i|
-  #     section = Section.new(s.to_hash)
-  #     section.order = i
-  #     section.id_section_parent_origin = @id
-  #     section
-  #   end
-  # end
-
   def to_section_links_hash
     section_links.map.with_index do |sectionMap, i|
       h = sectionMap.to_hash
