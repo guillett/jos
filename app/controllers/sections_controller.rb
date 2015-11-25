7c3a06a7515718cc125828028b6701e5e1986cfc
Simon Lehericey <simon.lehericey@gmail.com>
2015-11-25 16:21:06 +0100

    use section_article_valid for section details

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,7 +2,7 @@ class SectionsController < ApplicationController
   def show
     @section = Section.find(params[:id])
 
-    @articles = @section.section_article_links.includes(:article).where(state: ['VIGUEUR', 'ABROGE_DIFF']).order(:order).map(&:article)
+    @articles = @section.section_article_links_valid.includes(:article).map(&:article)
 
     @sub_sections = @section.code.summary(@section.id).section_links_preloaded.map(&:target)
   end
