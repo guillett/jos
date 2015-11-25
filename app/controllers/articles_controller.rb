0084b4d9029ab21573def5e26e9987cd5d50ced3
Nissim Naccache <nissimnac@gmail.com>
2015-11-25 18:30:11 +0100

    Improve++ diff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,19 @@ class ArticlesController < ApplicationController
   def diff
     @article_1 = Article.find_by(id_article_origin: params[:article_1_id])
     @article_2 = Article.find_by(id_article_origin: params[:article_2_id])
-    @diff_array = diff_to_array(@article_1.text, @article_2.text).map { |line| line.gsub(/\n/, "<br/>").gsub(/<br\/>-/, "<br/>").gsub(/<br\/>\+/, "<br/>") }
+
+    article_1_text = ""
+    @article_1.text.each_line {|line|  article_1_text += line.strip + "\n"}
+
+    article_2_text = ""
+    @article_2.text.each_line {|line|  article_2_text += line.strip + "\n"}
+
+    @diff_array = diff_to_array(article_1_text.strip.chomp, article_2_text.strip.chomp).map { |line| line.gsub(/\n/, "<br/>").gsub(/<br\/>-/, "<br/>").gsub(/<br\/>\+/, "<br/>") }
   end
 
   private
 
   def diff_to_array(article_1, article_2)
-    article_1 = article_1.gsub( /<br\/?>/, "\n")
-    article_2 = article_2.gsub( /<br\/?>/, "\n")
     Diffy::Diff.new(article_1, article_2).each_chunk.to_a
   end
 
