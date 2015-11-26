4701cfc086f7a21be5dd5793b9486e805c78f12c
Benoit Lafontaine <joel1di1@gmail.com>
2015-11-26 10:59:59 +0100

    Test page caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_cache_control_headers
+
+  def set_cache_control_headers
+    expires_in 1.day, :public => true
+  end
 end
