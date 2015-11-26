3febc1266881cf5157fc41eab79bf869b7e24eb3
Benoit Lafontaine <joel1di1@gmail.com>
2015-11-26 16:54:31 +0100

    remove cache control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_cache_control_headers
-
-  def set_cache_control_headers
-    expires_in 1.day, :public => true
-  end
+  
 end
