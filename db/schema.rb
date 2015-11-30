bb841941633e11374f8da70b6239d94f40522be2
Nissim Naccache <nissimnac@gmail.com>
2015-11-30 12:26:48 +0100

    Create keyword model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130101100) do
+ActiveRecord::Schema.define(version: 20151130112234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,12 @@ ActiveRecord::Schema.define(version: 20151130101100) do
     t.datetime "text_date"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "section_article_links", id: false, force: :cascade do |t|
