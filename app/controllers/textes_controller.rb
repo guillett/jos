6e55a1e8c0ca41d1b4dd18c9612376908d0b7c7e
Nissim Naccache <nissimnac@gmail.com>
2015-12-01 14:26:10 +0100

    Display jorftext using his id_jorftext_origin

--- app/controllers/textes_controller.rb
+++ app/controllers/textes_controller.rb
@@ -1,6 +1,6 @@
 class TextesController < ApplicationController
   def show
-    @jtext= Jtext.find(params[:id])
+    @jtext= Jtext.find_by(id_jorftext_origin: params[:id])
     @jorfcont_jtext_link = JorfcontJtextLink.find_by_jtext_id(@jtext.id)
   end
 end
