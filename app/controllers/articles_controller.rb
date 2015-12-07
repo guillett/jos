90a840cd43ec2592c8bb944e5f835ba96425e639
Nissim Naccache <nissimnac@gmail.com>
2015-12-07 14:14:46 +0100

    Display diff even if no jarticle is related

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,9 +16,7 @@ class ArticlesController < ApplicationController
 
     @diff_array = diff_to_array(article_1_text.strip.chomp, article_2_text.strip.chomp).map { |line| line.gsub(/\n/, "<br/>").gsub(/<br\/>-/, "<br/>").gsub(/<br\/>\+/, "<br/>") }
 
-    @history_link_modif = HistoryLink.where(article_id: @article_1.id, text_type: "MODIFICATION").first
-    jarticle_modif_id = JtextJarticleLink.includes(:jtext).where(jtexts: { id_jorftext_origin: @history_link_modif.id_text_origin }, jtext_jarticle_links: { order: @history_link_modif.order }).first.jarticle_id
-    @jarticle_modif = Jarticle.find(jarticle_modif_id)
+    @jarticle_modif = get_article_modificator
   end
 
   private
@@ -27,4 +25,13 @@ class ArticlesController < ApplicationController
     Diffy::Diff.new(article_1, article_2, :allow_empty_diff => false).each_chunk.to_a
   end
 
+  def get_article_modificator
+    @history_link_modif = HistoryLink.where(article_id: @article_2.id, text_type: ["MODIFICATION", "MODIFIE"]).first
+    return nil if @history_link_modif.nil?
+
+    jarticle_modif = JtextJarticleLink.includes(:jtext).where(jtexts: { id_jorftext_origin: @history_link_modif.id_text_origin }, jtext_jarticle_links: { order: @history_link_modif.order }).first
+    return nil if jarticle_modif.nil?
+
+    Jarticle.find(jarticle_modif.jarticle_id)
+  end
 end
