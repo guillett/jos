522a9d0f55df6a11702a670817f7ed3b09dbb21f
Nissim Naccache <nissimnac@gmail.com>
2015-12-10 10:53:46 +0100

    Improve Jarticle display

--- app/controllers/jarticles_controller.rb
+++ app/controllers/jarticles_controller.rb
@@ -1,5 +1,8 @@
 class JarticlesController < ApplicationController
   def show
     @jarticle = Jarticle.where(id_jarticle_origin: params[:id_jarticle_origin]).first
+    @jarticle_text = ""
+    @jarticle.text.each_line {|line|  @jarticle_text += line.strip + "\n"}
+    @jarticle_text = @jarticle_text.strip.chomp.gsub(/\n/, "<br/>").gsub(/« Art/, "<br/>« Art")
   end
 end
